9047eff3eb10f1d20e4aae7133609731cc50cd96
FuaI <imfuaison@gmail.com>
2025-01-15 12:48:59 -0800

    Update input form and requirements for fraud prediction app

--- tests/model.py
+++ tests/model.py
@@ -6,8 +6,20 @@ from sklearn.linear_model import LogisticRegression
 df = pd.read_csv('011525updated data.csv')
 
 # Select features and target
-X = df.iloc[:, 1:-1]
-y = df.iloc[:, -1]
+# Features (excluding 'ClaimID' and 'BeneID' as they are identifiers)
+X = df[['InscClaimAmtReimbursed', 'DeductibleAmtPaid',
+       'RenalDiseaseIndicator', 'ChronicCond_Alzheimer',
+       'ChronicCond_Heartfailure', 'ChronicCond_KidneyDisease',
+       'ChronicCond_Cancer', 'ChronicCond_ObstrPulmonary',
+       'ChronicCond_Depression', 'ChronicCond_Diabetes',
+       'ChronicCond_IschemicHeart', 'ChronicCond_Osteoporasis',
+       'ChronicCond_rheumatoidarthritis', 'ChronicCond_stroke',
+       'AdmissionDays', 'ClaimDays', 'is_dead', 'Num_UniqueClmDiagnosis',
+       'Num_UniqueProcedureCode', 'NumPhysicians']]
+
+# Target column
+y = df['PotentialFraud']  # Assuming 'Fraud' is your target column
+
 
 # Create the model instance
 model = LogisticRegression(class_weight='balanced', random_state=5)
